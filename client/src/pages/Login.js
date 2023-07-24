56a8dcdfb7eff0adc43a6caa5e1465c24fe20967
janengcodes <janeng@umich.edu>
2023-07-24 17:41:45 -0600

    login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -16,18 +16,20 @@ function Login({setIsAuth}) {
         });  
     };
   return (
-    <div className ="loginPage">
-        <div>
-            <h2>Sign In With Google To Continue</h2>
+    <div className="body">
+      <div className="loginPage">
+        <div className='text'>
+          <h2>LOGIN OR SIGN UP WITH GOOGLE</h2>
         </div>
-        <div>
-            {/* <Link to='/post' className='navButton'> POST </Link> */}
-            <div className="login-with-google-btn" onClick={signInWithGoogle}>Sign In</div>
-
+        <div className='button-container2'>
+          {/* <Link to='/post' className='navButton'> POST </Link> */}
+          <div className="login-with-google-btn" onClick={signInWithGoogle}>
+            Login/Sign Up
+          </div>
         </div>
-        
+      </div>
     </div>
-  )
+  );
 }
 
 signOut(auth).then(() => {
